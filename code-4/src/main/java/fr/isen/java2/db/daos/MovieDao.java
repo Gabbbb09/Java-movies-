4e43d45cd23203b1a3647b020984b7af97971996
gabriel <gillardgabriel4@gmail.com>
2024-02-09 09:43:25 +0100

    java movies

---
+++ code-4/src/main/java/fr/isen/java2/db/daos/MovieDao.java
@@ -0,0 +1,58 @@
+package fr.isen.java2.db.daos;
+
+import static fr.isen.java2.db.daos.DataSourceFactory.getDataSource;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.isen.java2.db.entities.Genre;
+import fr.isen.java2.db.entities.Movie;
+
+public class MovieDao {
+	
+	
+    
+	public List<Movie> listMovies() {
+        List<Movie> movies = new ArrayList<>();
+        String query = "SELECT * FROM movie JOIN genre ON movie.genre_id = genre.idgenre";
+        try (Connection connection = getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            
+            while (resultSet.next()) {
+                Movie movie = new Movie();
+                movie.setId(resultSet.getInt("idmovie"));
+                movie.setTitle(resultSet.getString("title"));
+                movie.setReleaseDate(resultSet.getDate("release_date").toLocalDate());
+                movie.setGenre(new Genre (resultSet.getInt("genre_id"),resultSet.getString("name")).getId());
+                movie.setDuration(resultSet.getInt("duration"));
+                movie.setDirector(resultSet.getString("director"));
+                movie.setSummary(resultSet.getString("summary"));
+                
+                
+                
+                
+                movies.add(movie);
+            }
+            return movies;
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur lors de la récupération des genres", e);
+        }
+        
+    }
+		    
+	
+
+	public List<Movie> listMoviesByGenre(String genreName) {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+
+	public Movie addMovie(Movie movie) {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+}
